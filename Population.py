d6e874e9fc585b5d9bc8a5b6ca2dfa0b21d1e5a4
FaizulMdnoor <faizul.mdnoor@novasourcepower.com>
2026-05-22 12:06:11 +0800

    kategory umur - age_cat = {         '1. kanak-kanak': ['0-4', '5-9'],         '2. remaja': ['10-14', '15-19'],         '3. dewasa_muda': ['20-24', '25-29', '30-34', '35-39'],         '4. dewasa_pertengahan': ['40-44', '45-49', '50-54', '55-59'],         '5. wargaemas': ['60-64', '65-69', '70-74', '75-79', '80-84', '85+', '70+']     }

--- Population.py
+++ Population.py
@@ -2,6 +2,38 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
+
+def age_catergory(df_pop_age: pd.DataFrame):
+    age_cat = {
+        '1. kanak-kanak': ['0-4', '5-9'],
+        '2. remaja': ['10-14', '15-19'],
+        '3. dewasa_muda': ['20-24', '25-29', '30-34', '35-39'],
+        '4. dewasa_pertengahan': ['40-44', '45-49', '50-54', '55-59'],
+        '5. wargaemas': ['60-64', '65-69', '70-74', '75-79', '80-84', '85+', '70+']
+    }
+
+    age_map = {}
+    for category, ages in age_cat.items():
+        for age in ages:
+            age_map[age] = category
+    df_pop_age['age_category'] = df_pop_age['age'].map(age_map)
+
+    df_filtered = df_pop_age.dropna(subset=['age_category'])
+    df_result = (
+        df_filtered
+        .groupby(['date','age_category'])['population']
+        .sum()
+        .reset_index()
+    )
+    df_pivot_results = (
+        df_result
+        .pivot_table(index='date', columns='age_category', values='population', aggfunc='sum')
+        .fillna(0)
+    )
+    df_pivot_results['total'] = df_pivot_results.sum(axis=1)
+    df_pivot_results.reset_index(inplace=True)
+    return df_pivot_results
+
 def get_population():
     url_population = 'https://storage.dosm.gov.my/population/population_malaysia.parquet'
     df_pop = pd.read_parquet(url_population)
@@ -45,6 +77,8 @@ def population_by_ages(df_population: pd.DataFrame):
     df_pop_by_ages = df[['date', 'age', 'population']].copy()
     df_pop_by_ages['date'] = pd.to_datetime(df_pop_by_ages['date'], format='%Y').dt.strftime('%Y')
 
+    population_age_cat = age_catergory(df_pop_by_ages)
+
     df_ages = (
         df_pop_by_ages
         .pivot_table(index='date', columns='age', values='population', aggfunc='sum')
@@ -53,13 +87,13 @@ def population_by_ages(df_population: pd.DataFrame):
 
     df_ages['total'] = df_ages.sum(axis=1)
     df_ages = df_ages.reset_index()
-    return df_ages
+    return df_ages, population_age_cat
 
 df_population = get_population()
 df_population['population'] = df_population['population'].astype(int) * 1000
 df_Mal_pop = population_by_year(df_population=df_population)
 df_male_pop, df_female_pop = population_by_gender(df_population=df_population)
-df_by_ages = population_by_ages(df_population=df_population)
+df_by_ages, df_age_category = population_by_ages(df_population=df_population)
 df_pop_by_gender = pd.merge(df_female_pop, df_male_pop, on='date', how='left')
 df_pop_by_gender['total'] = df_pop_by_gender['male'] + df_pop_by_gender['female']
 
@@ -107,6 +141,24 @@ fig1.update_traces(
 fig1.update_layout(yaxis=dict(tickformat='.2s'))
 
 fig1.show()
+
+fig_age_category = px.bar(
+    df_age_category,
+    x='date',
+    y=df_age_category.columns[1:-1],
+    title='Populasi Mengikut Kategori Umur',
+    labels={
+        'date': 'Tahun',
+        'value': 'Populasi',
+        'variable': 'Kumpulan Umur'
+    }
+)
+
+fig_age_category.update_layout(
+    barmode='stack',
+    yaxis=dict(tickformat='.2s')
+)
+fig_age_category.show()
 
 fig2 = px.bar(
     df_by_ages,
